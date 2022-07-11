b77847c077b68568dc24a3bb74d25a61c38a8243
BigBadDumper <93626098+BigBadDumper@users.noreply.github.com>
2022-07-11 22:52:54 +1000

    initial commit
    I wanna die...

---
+++ ConsoleCreatures.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //going to make a ghost thing
+      Console.WriteLine(" .-------.");
+      Console.WriteLine("(  o   o  )");
+      Console.WriteLine("|    O    |");
+      Console.WriteLine("|( . Y . )|");
+      Console.WriteLine("'^^^^^^^^^'");
+      Console.WriteLine("Big titty ghost GF");
+     }
+  }
+}
